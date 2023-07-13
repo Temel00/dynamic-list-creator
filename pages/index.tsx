804171a1cf1eaf638c5ff82631b5e94ccc78a7c9
Tyler Emel <53627007+Temel00@users.noreply.github.com>
2023-07-13 08:39:19 +0000

    Fixed protected page props, added footer to pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,47 +11,20 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Next.js site on GitHub Pages</title>
+        <title>Habit Sherpa</title>
         <meta
           name="description"
-          content="This is a Next.js template that can be exported as a static website and deployed on GitHub Pages."
+          content="This is a dynamic list creation app that lets users build todos, routines, and habits."
         />
-        <link rel="icon" href="./favicon.ico" />
+        <link rel="icon" href="./backpack.ico" />
       </Head>
 
       <main className={styles.main}>
         <Auth />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-        <p className={styles.description}>
-          <a href="https://github.com/ethanppl/gh-pages-nextjs">
-            This is a Next.js project template with typescript
-          </a>{' '}
-          that can be exported as a static website.
-        </p>
-        <p className={styles.description}>
-          Try using this as a{' '}
-          <a href="https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template">
-            template
-          </a>{' '}
-          to publish your Next.js site on{' '}
-          <a href="https://docs.github.com/en/pages/getting-started-with-github-pages/about-github-pages">
-            GitHub pages
-          </a>
-          !
-        </p>
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-        </div>
+        {isLoggedIn 
+        ? (<div style={{marginTop:'5em'}}><h1>This is the home page for {(user as any)?.displayName}</h1></div>)
+        : (<div style={{marginTop:'5em'}}><h1>Please Login to view your lists.</h1></div>)
+        }
       </main>
       <Footer />
     </div>
